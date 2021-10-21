63bff0cb7a7f19aface99f6f2518fdcc0d68c730
moefear85 <moefear85@gmail.com>
2021-10-21 15:09:27 +0200

    Cleaned up uartSetup()

--- main/uart.c
+++ main/uart.c
@@ -4,17 +4,16 @@ static QueueHandle_t uart1_queue;
 
 void uartSetup()
 {
-    uart_config_t uart_config;
-    uart_config.baud_rate = 115200;
-    uart_config.data_bits = UART_DATA_8_BITS;
-    uart_config.parity = UART_PARITY_DISABLE;
-    uart_config.stop_bits = UART_STOP_BITS_1;
-    uart_config.flow_ctrl = UART_HW_FLOWCTRL_DISABLE;
-    uart_config.rx_flow_ctrl_thresh = 0;
+    uart_config_t uart_config
+    {
+        .baud_rate = 115200,
+        .data_bits = UART_DATA_8_BITS,
+        .stop_bits = UART_STOP_BITS_1,
+    };
     
     ESP_ERROR_CHECK( uart_param_config(UART_NUM_1, &uart_config));
-    ESP_ERROR_CHECK( uart_set_pin(1, 12, 11, 17, 18));
-    ESP_ERROR_CHECK( uart_driver_install(UART_NUM_1, 1024, 1024, 10, &uart1_queue, 0) );
+    ESP_ERROR_CHECK( uart_set_pin(UART_NUM_1, 12, 11, 17, 18));
+    ESP_ERROR_CHECK( uart_driver_install(UART_NUM_1, 1024, 1024, 0, NULL, 0) );
 }
 
 void uartLoop()
